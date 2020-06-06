52586a184b7cada9a0796b9faab90bf87edbe78c
Maxim Dick <maxim1994barca@gmail.com>
2020-06-06 20:34:16 +0300

    first commit application Movies

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
 "use strict";
+/*
+* Задание на урок:
+*
+* 1) создать переменную numberOfFilms и в нее поместить ответ от пользователя на вопрос:
+* "сколько фильмов вы уже посмотрели?"
+*
+* 2) Создать объект personalMovieDB и в него постить такие свойства:
+* - count - сюда передается ответ на первый вопрос
+* - movies - в это свойство поместить пустой объект
+* - actors - тоже поместить пустой объект
+* - genres - сюда поместить пустой массив
+* - privat - в это свойство поместить boolean(логическое) значение false. **/
 
-let incr = 10;
-let decr = 10;
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actor : {},
+    genres : [],
+    privat : false
+};
+
+const lastWatchFilm1 = prompt("Один из последних просмотренних фильмов?", "");
+const ballFilm1 = +prompt("Насколько оцените его?", "");
+const lastWatchFilm2 = prompt("Один из последних просмотренних фильмов?", "");
+const ballFilm2 = +prompt("Насколько оцените его?", "");
+
+personalMovieDB.movies[lastWatchFilm1] = ballFilm1;
+personalMovieDB.movies[lastWatchFilm2] = ballFilm2;
 
 
-console.log(5 < 2 || 3 < 1);
-
-
+console.log(personalMovieDB)
